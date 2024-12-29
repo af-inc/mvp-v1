983a466a4a0749bc55ebdad5a831edd1737c7ddd
Christian-Madu Somtochukwu <cm.somto@gmail.com>
2024-12-29 19:49:22 +0100

    3rd commit by phemi

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -1,30 +1,30 @@
-import { NextRequest, NextResponse } from 'next/server';
-import clientPromise from '@/app/lib/db';
-import { comparePasswords, generateJWT } from '@/app/lib/auth';
+// import { NextRequest, NextResponse } from 'next/server';
+// import clientPromise from '@/app/lib/db';
+// import { comparePasswords, generateJWT } from '@/app/lib/auth';
 
-interface User {
-  useremail: string;
-  userpassword: string;
-}
+// interface User {
+//   useremail: string;
+//   userpassword: string;
+// }
 
-export async function POST(request: NextRequest) {
-  const { useremail, userpassword }: User = await request.json();
+// export async function POST(request: NextRequest) {
+//   const { useremail, userpassword }: User = await request.json();
 
-  const client = await clientPromise;
-  const db = client.db();
-  const usersCollection = db.collection<User>('users');
+//   const client = await clientPromise;
+//   const db = client.db();
+//   const usersCollection = db.collection<User>('users');
 
-  const user = await usersCollection.findOne({ useremail });
-  if (!user) {
-    return NextResponse.json({ error: 'Invalid email or password' }, { status: 400 });
-  }
+//   const user = await usersCollection.findOne({ useremail });
+//   if (!user) {
+//     return NextResponse.json({ error: 'Invalid email or password' }, { status: 400 });
+//   }
 
-  const isPasswordValid = await comparePasswords(userpassword, user.userpassword);
-  if (!isPasswordValid) {
-    return NextResponse.json({ error: 'Invalid email or password' }, { status: 400 });
-  }
+//   const isPasswordValid = await comparePasswords(userpassword, user.userpassword);
+//   if (!isPasswordValid) {
+//     return NextResponse.json({ error: 'Invalid email or password' }, { status: 400 });
+//   }
 
-  const token = generateJWT(user._id.toString());
+//   const token = generateJWT(user._id.toString());
 
-  return NextResponse.json({ message: 'Signin successful', token });
-}
+//   return NextResponse.json({ message: 'Signin successful', token });
+// }
